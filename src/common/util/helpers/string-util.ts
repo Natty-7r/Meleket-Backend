5a8ec22b88eac5bcf20b2be207aa8aa9dbc8a403
Natty-7 <nati7fekadu@gmail.com>
2024-08-25 23:17:53 +0300

    feat: add custom string to lowercase && update capitalize method

--- src/common/util/helpers/string-util.ts
+++ src/common/util/helpers/string-util.ts
@@ -3,7 +3,15 @@ export const changeSpaceByHypen = (text: string): string => {
 }
 
 export const capitalize = (text: string): string =>
-  text[0].toLocaleUpperCase().concat(text.slice(1))
+  text
+    .split(' ')
+    .map((word) => word.toLocaleUpperCase())
+    .reduce((textCatitalized, wordCapitalized) =>
+      textCatitalized.concat(' ' + wordCapitalized),
+    )
+
+export const tolowercaseCustom = (text: string): string =>
+  text.toLocaleLowerCase().trim()
 
 export const generateVerifyEmailOTPMessage = ({
   firstName,
