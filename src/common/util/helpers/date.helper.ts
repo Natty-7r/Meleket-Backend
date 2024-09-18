8408eb8b027204d079f7511cc1c99138c0a38be7
Natty-7r <nati7fekadu@gmail.com>
2024-09-18 13:26:17 +0300

    fix: update package expire date calculator

--- src/common/util/helpers/date.helper.ts
+++ src/common/util/helpers/date.helper.ts
@@ -1,23 +1,5 @@
-import { PackageType } from '@prisma/client'
-
-export const calculatePackageExpireDate = (packageType: PackageType): Date => {
-  let packageDuration = 0
-  switch (packageType) {
-    case 'MONTHLY':
-      packageDuration = 30
-      break
-    case 'THREE_MONTHLY':
-      packageDuration = 90
-      break
-    case 'SIX_MONTHLY':
-      packageDuration = 180
-      break
-    case 'YEARLY':
-      packageDuration = 366
-      break
-    default:
-      packageDuration = 1
-  }
+export const calculatePackageExpireDate = (monthCount: number): Date => {
+  const packageDuration = monthCount * 30
 
   const expireDate = new Date(
     new Date().setDate(new Date().getDate() + packageDuration),
