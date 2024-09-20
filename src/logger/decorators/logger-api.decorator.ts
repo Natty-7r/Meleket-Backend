b2e8e79e7f4e46e1bddb8c421d187ff11d999cdd
Natty-7 <nati7fekadu@gmail.com>
2024-09-20 23:32:08 +0300

    chore: update read access to admin

--- src/logger/decorators/logger-api.decorator.ts
+++ src/logger/decorators/logger-api.decorator.ts
@@ -2,7 +2,6 @@ import { applyDecorators } from '@nestjs/common'
 
 import Roles from 'src/common/decorators/roles.decorator'
 import { ViewLogsSwaggerDefinition } from './logger-swagger.decorator'
-import Public from 'src/common/decorators/public.decorator'
 
 export const ViewLogs = () =>
-  applyDecorators(Public(), ViewLogsSwaggerDefinition())
+  applyDecorators(Roles('ADMIN', 'SUPER_ADMIN'), ViewLogsSwaggerDefinition())
